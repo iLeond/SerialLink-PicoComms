9b5c9ac3789eb024770d99a87ac982810a2362cb
iLeond <ileonrd@hotmail.com>
2024-02-12 00:06:51 -0800

    Funcionamiento con picos multimples a un solo comando de python

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import machine
 import utime
+# En Pico 1, usa "PICO_ID_1"; en Pico 2, usa "PICO_ID_2"
+PICO_ID = "PICO_ID_2"  # Cambia este valor para el segundo Pico
 
 class PicoUART:
     def __init__(self, baudrate=9600):
         self.uart = machine.UART(0, baudrate=baudrate)
     
     def send_greeting(self):
-        message = "Hola desde la Pico\n"
+        message = f"{PICO_ID}: Hola desde la Pico\n"
         self.uart.write(message)
-        print(message.strip())  # Para depuración
+        print(message.strip())
 
     def listen_for_data(self):
         if self.uart.any():
             received_data = self.uart.read().decode('utf-8').strip()
-            print(f"Dato recibido: {received_data}")  # Confirmación de la recepción para depuración
-            response_message = f"Dato recibido: {received_data}\n"
-            self.uart.write(response_message.encode('utf-8'))  # Asegúrate de codificar la respuesta
+            print(f"Dato recibido: {received_data}")
+            response_message = f"{PICO_ID}: Dato recibido: {received_data}\n"
+            self.uart.write(response_message.encode('utf-8'))
 
     def run(self):
         while True:
